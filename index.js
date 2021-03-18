6caddb868d59dd55caec1b168b6c07595a45bf46
Camilo Perona <peronacamilo@gmail.com>
2021-03-17 22:48:32 -0300

    fixed cors

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 
 const cors = require('cors')
+
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/routes');
+
 //Express
 const server = express ();
 var corsOptions = {
-    origin: 'http://localhost:5000',
+    origin: 'http://107.180.107.29:5000',
     optionsSuccessStatus: 200 // For legacy browser support
 }
 server.use(cors(corsOptions))
-
-//Body Parser
-const bodyParser = require('body-parser');
-
-//Conexion con archivos de rutas
-const routes = require('./routes/routes');
 
 //Inicializar servidor
 server.listen(3001, () => {
